93190628e926146fa6bed7a053d53fca8ba9c2a0
Rocky Lai <99881874+WingLai852@users.noreply.github.com>
2025-10-16 11:36:53 +0200

    bmi calculator

--- bmi-calculator/task.py
+++ bmi-calculator/task.py
@@ -1,5 +1,7 @@
-height = 1.75
-weight = 62.9
+height = float(input("What is your height in cm? "))
+weight = float(input("What is your weight in kg? "))
 
-bmi = weight / height**2
-print(bmi)
+height_m = height / 100
+bmi = weight / (height_m ** 2)
+
+print(f"Your BMI is {round(bmi, 2)}")
